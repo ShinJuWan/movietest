750e0317dba02d6ec17823487a37bd26534c5511
Shin Ju Wan <ariel8347@nate.com>
2021-09-14 14:22:04 +0900

    test 1

---
+++ movie_api.py
@@ -0,0 +1,22 @@
+from movie_model import MovieModel
+import requests
+
+def callMovieApi(page=1):
+    url = f'''
+        https://yts.mx/api/v2/list_movies.json?sort_by=rating&page_number={page}&limit=20
+    '''
+    response = requests.get(url)
+
+    responseDict = response.json() 
+    movies = responseDict["data"]["movies"] #movies = list type
+    return convert_model(movies)
+
+
+def convert_model(movies):
+    list = []    
+
+    for movie in movies:
+        movie_model = MovieModel(movie["title"], movie["rating"], movie["small_cover_image"], movie["summary"])
+        list.append(movie_model)
+
+    return list
