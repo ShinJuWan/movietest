cdb4950e51c629a701714578a871c9b8b0f2c85d
Shin Ju Wan <ariel8347@nate.com>
2021-09-14 17:52:26 +0900

    last

--- weather_api.py
+++ weather_api.py
@@ -1,15 +1,14 @@
-from weather_model import WeatherModel
 import requests
 from bs4 import BeautifulSoup	
 
-url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=4&ie=utf8&query=%EB%82%A0%EC%94%A8"
+def getTemp():
+    url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=4&ie=utf8&query=%EB%82%A0%EC%94%A8"
+    response = requests.get(url)
 
-response = requests.get(url)
-
-if (response.status_code == 200):
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
-    temps = soup.find(["span"],["todaytemp"])
-    print(temps)
+    temp = soup.find('span',"todaytemp").get_text()
+    return temp
+
 
 
--- weather_model.py
+++
@@ -1,5 +0,0 @@
-class WeatherModel:
-    def __init__(self, temps):
-        self.temps = temps
-
-
